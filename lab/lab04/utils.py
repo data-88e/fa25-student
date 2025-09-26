9fa2befcd66f4993f97d46f90638c46f93b42c66
Justin Wang <wjustin9@berkeley.edu>
2025-09-25 20:03:18 -0700

    lab 4 otterized

--- lab/lab04/utils.py
+++ lab/lab04/utils.py
@@ -6,19 +6,19 @@ import plotly.graph_objs as go
 from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 from ipywidgets import interact, interactive, fixed, interact_manual
 
-def cobb_douglas_sol(A, K, L, alpha, beta = None):
+def cobb_douglas(A, K, L, alpha, beta = None):
     if beta is None:
         return A * K ** alpha * L ** (1 - alpha)
     else:
         return A * K ** alpha * L ** (beta)
         
-def cobb_douglas_plotter_K_sol(A, L_bar, alpha, y):
+def cobb_douglas_plotter_K(A, L_bar, alpha, y):
     plt.plot(np.arange(0, 1.01, 0.01), y)
     plt.title(fr"Cobb-Douglas with $\bar L$ = {L_bar}, $A$ = {A} and $\alpha$ = {alpha}")
     plt.xlabel("Capital Stock")
     plt.ylabel("Output");
     
-def cobb_douglas_plotter_L_sol(A, K_bar, alpha):
+def cobb_douglas_plotter_L(A, K_bar, alpha):
     L_s = np.arange(0, 1.01, 0.01)
     V_3 = cobb_douglas(A, K_bar, L_s, alpha)
     plt.plot(L_s, V_3)
@@ -26,7 +26,7 @@ def cobb_douglas_plotter_L_sol(A, K_bar, alpha):
     plt.xlabel("Labor Force")
     plt.ylabel("Output");
     
-def plot_cobb_douglas_sol(V, orig_V):
+def plot_cobb_douglas(V, orig_V):
     data = [go.Surface(z = V, contours = go.surface.Contours(z = go.surface.contours.Z(show = False, project = dict(z = True))),
                       colorscale = "Electric", showscale = False),
            go.Surface(z = orig_V, contours = go.surface.Contours(z = go.surface.contours.Z(show = False, project = dict(z = True))),
@@ -36,7 +36,7 @@ def plot_cobb_douglas_sol(V, orig_V):
     fig = go.Figure(data = data, layout = layout)
     iplot(fig)
     
-def orig_cobb_douglas_sol():
+def orig_cobb_douglas():
     L_s = np.arange(0, 10.11, 0.1)
     K_s = np.arange(0, 10.11, 0.1)
     A = 1
@@ -45,7 +45,7 @@ def orig_cobb_douglas_sol():
     curr_V = cobb_douglas(A, xx, yy, alpha)
     return curr_V
 
-def change_A_sol(A):
+def change_A(A):
     L_s = np.arange(0, 10.11, 0.1)
     K_s = np.arange(0, 10.11, 0.1)
     alpha = 0.5
@@ -53,7 +53,7 @@ def change_A_sol(A):
     curr_V = cobb_douglas(A, xx, yy, alpha)
     plot_cobb_douglas(curr_V, orig_cobb_douglas())
     
-def change_alpha_sol(alpha):
+def change_alpha(alpha):
     L_s = np.arange(0, 10.11, 0.1)
     K_s = np.arange(0, 10.11, 0.1)
     A = 1
@@ -61,7 +61,7 @@ def change_alpha_sol(alpha):
     curr_V = cobb_douglas(A, xx, yy, alpha)
     plot_cobb_douglas(curr_V, orig_cobb_douglas())
 
-def change_alpha_beta_sol(alpha_beta_sum):
+def change_alpha_beta(alpha_beta_sum):
     L_s = np.arange(0, 10.11, 0.1)
     K_s = np.arange(0, 10.11, 0.1)
     A = 1
